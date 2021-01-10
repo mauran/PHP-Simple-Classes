c7af27e2458e9dd64776cea91d9f63bddee7c19a
Alex Westergaard <aw.freelancer@gmail.com>
2021-01-10 15:07:08 +0100

    Added basic Open Graph Elements and Blocks

--- html.php
+++ html.php
@@ -14,8 +14,23 @@ class HTML {
     static private array $script = [];
     static private array $scriptBody = [];
 
+    // Use index 0 for default URL in arrays, so ['url', 'attr'=>'value'] or ['url']
+    const OPG_IMAGE = ['secure_url', 'type', 'width', 'height', 'alt'];
+    const OPG_VIDEO = ['secure_url', 'type', 'width', 'height'];
+    const OPG_AUDIO = ['secure_url', 'type'];
+    static private array $opengraph = [
+        'og:url' => '',
+        'og:type' => 'website',
+        'og:title' => '',
+        'og:description' => '',
+    ];
+    static private array $opengraphBlocks = [];
+
     static public function setTitle (string $title) {
         self::$title = $title;
+        if (strlen(self::$opengraph['og:title'])<1) {
+            self::$opengraph['og:title'] = $title;
+        }
     }
 
     static public function setTitleEnd (string $title_end) {
@@ -24,10 +39,14 @@ class HTML {
 
     static public function setDescription (string $description) {
         self::$description = $description;
+        if (strlen(self::$opengraph['og:description'])<1) {
+            self::$opengraph['og:description'] = $title;
+        }
     }
 
     static public function setCanonical (string $url) {
         self::$canonical = $url;
+        self::$opengraph['og:url'] = $url;
     }
 
     static public function setRobots (array $tags) {
@@ -68,9 +87,12 @@ class HTML {
         $tags_script_body = [
             ...self::$scriptBody
         ];
+        $tags_opengraph = self::$opengraph;
+        $tags_opengraph_blocks = self::$opengraphBlocks;
         $buffer = [
-            '<title>'. strip_tags(trim(self::$title)) . strip_tags(trim(self::$title_end)) .'</title>'
+            '<title>'. strip_tags(trim(self::$title)) . strip_tags(rtrim(self::$title_end)) .'</title>'
         ];
+        // Basic meta tags
         foreach ($tags_meta as $meta) {
             $tag = '<meta ';
             foreach ($meta as $attr => $value) {
@@ -79,6 +101,18 @@ class HTML {
             $tag .= '/>';
             $buffer[] = $tag;
         }
+        // Opengraph
+        foreach($tags_opengraph as $property => $content) {
+            $tag = '<meta property="'.$property.'" content="'.$content.'" />';
+            $buffer[] = $tag;
+        }
+        foreach($tags_opengraph_blocks as $opengraph) {
+            foreach($opengraph as $property => $content) {
+                $tag = '<meta property="'.$property.'" content="'.$content.'" />';
+                $buffer[] = $tag;
+            }
+        }
+        // Basic Links
         foreach ($tags_link as $link) {
             $tag = '<link ';
             foreach ($link as $attr => $value) {
@@ -87,12 +121,13 @@ class HTML {
             $tag .= '/>';
             $buffer[] = $tag;
         }
+        // Scripts
         foreach ($tags_script as $script) {
             $tag = '<script ';
             foreach ($script as $attr => $value) {
                 $tag .= $attr .'="'. htmlentities(trim($value)) .'" ';
             }
-            $tag .= '></script>';
+            $tag .= '/></script>';
             $buffer[] = $tag;
         }
         foreach ($tags_script_body as $body) {
@@ -113,6 +148,58 @@ class HTML {
             $final .= "\n\t</head>\n";
         }
         return $final;
+    }
+
+    static function setOpenGraphType (string $type) {
+        self::$opengraph['type'] = $type;
+    }
+
+    static function addOpenGraphImage ($data) {
+        if (is_array($data)) {
+            $blob = ['og:image'=>$data[0]];
+            foreach(self::OPG_IMAGE as $attr) {
+                if (isset($data[$attr])) {
+                    $blob['og:image:'. $attr] = $data[$attr];
+                }
+            }
+        } elseif (is_string($data) || is_float($data)) {
+            $blob = ['og:image'=>$data];
+        }
+        if ($blob) {
+            self::$opengraphBlocks[] = $blob;
+        }
+    }
+
+    static function addOpenGraphVideo ($data) {
+        if (is_array($data)) {
+            $blob = ['og:video'=>$data[0]];
+            foreach(self::OPG_IMAGE as $attr) {
+                if (isset($data[$attr])) {
+                    $blob['og:video:'. $attr] = $data[$attr];
+                }
+            }
+        } elseif (is_string($data) || is_float($data)) {
+            $blob = ['og:video'=>$data];
+        }
+        if ($blob) {
+            self::$opengraphBlocks[] = $blob;
+        }
+    }
+
+    static function addOpenGraphAudio ($data) {
+        if (is_array($data)) {
+            $blob = ['og:audio'=>$data[0]];
+            foreach(self::OPG_IMAGE as $attr) {
+                if (isset($data[$attr])) {
+                    $blob['og:audio:'. $attr] = $data[$attr];
+                }
+            }
+        } elseif (is_string($data) || is_float($data)) {
+            $blob = ['og:audio'=>$data];
+        }
+        if ($blob) {
+            self::$opengraphBlocks[] = $blob;
+        }
     }
 
 }
